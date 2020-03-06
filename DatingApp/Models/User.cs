1191d6550e1c9bebb27ebd4668ff8e7b9a4757ee
hassan <hassanomran333@gmail.com>
2020-03-06 20:16:23 +0200

    Add files via upload

---
+++ DatingApp/Models/User.cs
@@ -0,0 +1,33 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DatingApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnowsAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        [JsonIgnore]
+        public ICollection<Photo> photos { get; set; }
+        public ICollection <Like> Likers { get; set; }
+        public ICollection <Like> Likees { get; set; }
+        public ICollection<Message> MessageSent { get; set; }
+        public ICollection<Message> MessageReceived { get; set; }
+
+    }
+}
