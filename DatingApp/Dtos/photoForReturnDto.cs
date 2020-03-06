773d3721132e86d3335fa1097d0824b0a9762a8d
hassan <hassanomran333@gmail.com>
2020-03-06 20:13:43 +0200

    Add files via upload

---
+++ DatingApp/Dtos/photoForReturnDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Dtos
+{
+    public class photoForReturnDto
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Descripion { get; set; }
+        public DateTime DateAdded { get; set; }
+        public bool IsMain { get; set; }
+        public string PublicId { get; set; }
+    }
+}
